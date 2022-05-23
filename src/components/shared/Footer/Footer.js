9e621ac5f8a1c90b6219b1af4bacdeccc201ba5f
Najmul890 <najmul890890@gmail.com>
2022-05-22 20:52:05 -0700

    header section done

---
+++ src/components/shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='p-5 text-white' >
+            <p className="fw-bold text-center"> Created by <span className="main-color">Md. Najmul Hossain Rajib </span>. &copy;2022, All rights reserved </p>
+        </div>
+    );
+};
+
+export default Footer;
