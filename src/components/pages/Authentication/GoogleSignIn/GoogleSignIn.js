de0049d3b5f8d980d6e10592beb3974e8a6a04d3
Najmul890 <najmul890890@gmail.com>
2022-05-24 10:10:03 -0700

    implemented sign in with google

---
+++ src/components/pages/Authentication/GoogleSignIn/GoogleSignIn.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Loading from '../../../shared/Loading/Loading';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { auth } from '../../../../firebase.init';
+
+const GoogleSignIn = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
+
+    let errorElement;
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message} </p>
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+    return (
+        <div>
+            <div className='d-flex align-items-center'>
+                <div style={{ height: '1px' }} className='element-bg w-50'></div>
+                <p className='mt-2 px-2'>or</p>
+                <div style={{ height: '1px' }} className='element-bg w-50'></div>
+            </div>
+            {errorElement}
+
+            <div>
+                <button onClick={() => signInWithGoogle()} className="btn btn-success text-white">Sign in with google</button>
+
+            </div>
+        </div>
+    );
+};
+
+export default GoogleSignIn;
