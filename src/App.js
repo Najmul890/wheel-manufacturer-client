353192fd54de9c06560416fcb684a7223ddada13
Najmul890 <najmul890890@gmail.com>
2022-05-24 09:41:00 -0700

    register page completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 import Login from './components/pages/Authentication/Login/Login';
+import Register from './components/pages/Authentication/Register/Register';
 import Home from './components/pages/Home/Home';
 import Header from './components/shared/Header/Header';
 
@@ -14,7 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>} ></Route>
         <Route path="/login" element={<Login></Login>} ></Route>
+        <Route path="/register" element={<Register></Register>} ></Route>
       </Routes>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
---
+++ src/components/pages/Authentication/Register/Register.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { auth } from '../../../../firebase.init';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { Form } from 'react-bootstrap';
+import { Button } from 'bootstrap';
+
+const Register = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || "/";
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    
+
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+
+    const [updateProfile] = useUpdateProfile(auth);
+
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        toast('email sent to verify your email address');
+        await createUserWithEmailAndPassword(email, password);
+
+        await updateProfile({ displayName: name });
+    }
+    return (
+        <div className='container w-lg-50 w-100 mx-auto mb-5' >
+            <h2 className='text-center mt-2'>Please Register</h2>
+                <Form onSubmit={handleRegister}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control name="name" type="text" placeholder="Your Name" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Control name="email" type="email" placeholder="Your email" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Control name="password" minLength={6} type="password" placeholder="Password" required />
+                    </Form.Group>
+                    <Button className="w-50 mx-auto d-block mb-2" type="submit">
+                        Register
+                    </Button>
+                </Form>
+                <p className='text-primary' >Already have an account? <Link className='text-success' to="/login" >Login</Link> </p>
+                
+        </div>
+    );
+};
+
+export default Register;
